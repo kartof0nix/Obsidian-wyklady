cd776bbd8509d8cf558a6ae28f28a8aaecfe4377
Sari Bonat <karol.bonatkarol@gmail.com>
2025-05-30 08:31:03 +0200

    vault backup: 2025-05-30 08:31:03

--- POEL/MATLAB/fdemo.m
+++ POEL/MATLAB/fdemo.m
@@ -1,7 +1,12 @@
 Fg = 48;
 
 Hd = @(f) (-1/Fg) * (exp(-i*2*pi*Fg / f) / (1 - (exp(-i*2*pi*Fg / f))));
-Ha = @(f) j / (2 * pi * f)
+Ha = @(f) 1j ./ (2 * pi .* f);
 
 Ha(1.3)
 Ha(5.89)
+
+xscale log
+yscale log
+grid on
+fplot(@(x) abs(Ha(x)), [0.1, 6])
