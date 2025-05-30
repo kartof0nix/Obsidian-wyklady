cd98484bb01cffc9c85e83dca57827ba763afbd0
Karol Bonat <01197226@pw.edu.pl>
2025-05-30 12:08:59 +0200

    vault backup: 2025-05-30 12:08:59

--- POEL/MATLAB/fdemo.m
+++ POEL/MATLAB/fdemo.m
@@ -1,6 +1,6 @@
 Fg = 48;
 
-Hd = @(f) (-1/Fg) * (exp(-i*2*pi*Fg / f) / (1 - (exp(-i*2*pi*Fg / f))));
+Hd = @(f) (-1/Fg) .* (exp(-1i*2*pi*Fg ./ f) ./ (1 - (exp(-1i*2*pi*Fg ./ f))));
 Ha = @(f) 1j ./ (2 * pi .* f);
 
 Db = @(x) 20 * log(x);
@@ -15,25 +15,27 @@ grid on
 in0 = [0.1, 6];
 in1 = [0.1, 24];
 in2 = [0.1, 96];
-tint = in0;
+tint = in2;
 
 tiledlayout(2,1)
 
 nexttile
 hold on
 fplot(@(x) Db(abs(Ha(x))), tint);
-fplot(@(x) Db(abs(Hz(x))), tint);
+fplot(@(x) Db(abs(Hd(x))), tint);
 ylabel("|H(f)| [dB]");
 xlabel("F [kHz]");
-legend("Ha(f)", "Hz(f)");
+legend("Ha(f)", "Hd(f)");
+grid on
 hold off
 
 nexttile
 hold on
 fplot(@(x) angle(Ha(x)), tint);
-fplot(@(x) anlge(Hz(x)), tint);
+fplot(@(x) angle(Hd(x)), tint);
 ylabel("arg(H(f)) ");
 xlabel("F [kHz]");
-legend("Ha(f)", "Hz(f)");
+legend("Ha(f)", "Hd(f)");
+grid on
 hold off
 
